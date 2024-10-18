df5849719202eea0ee7850f71d795c96a09d5964
Yousef Ali <you@example.com>
2024-10-18 12:13:08 +0300

    Add project files.

---
+++ BasicSaveWithTracking/Program.cs
@@ -0,0 +1,90 @@
+﻿
+using C01.BasicSaveWithTracking.Data;
+using C01.BasicSaveWithTracking.Entities;
+using C01.BasicSaveWithTracking.Helpers;
+
+namespace BasicSaveWithTracking
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //RunBasicSave();
+            //RunBasicUpdate();
+            //RunBasicDelete();
+            //RunMultipleOPWithSingleSave();
+            //RunRelatedData();
+
+        }
+
+        private static void RunBasicUpdate()
+        {
+          using(var context = new AppDbContext())
+            {
+                var author = context.Authors
+                    .FirstOrDefault(x => x.Id == 1);
+                author.FName = "Eric";
+                context.SaveChanges();
+            }
+        }
+        private static void RunBasicDelete()
+        {
+            using (var context = new AppDbContext())
+            {
+                var author = context.Authors
+                    .FirstOrDefault(x => x.Id == 1);
+
+               context.Authors.Remove(author);
+
+                context.SaveChanges();
+            }
+        }
+        private static void RunBasicSave()
+        {
+            DatabaseHelper.RecreateCleanDatabase();
+          
+            using (var context = new AppDbContext())
+            {
+                var author = new Author { Id = 1, FName = "eric", LName = "Evan" };
+                context.Authors.Add(author);
+                context.SaveChanges();
+            }
+        }
+        private static void RunMultipleOPWithSingleSave()
+        {
+            DatabaseHelper.RecreateCleanDatabase();
+
+            using (var context = new AppDbContext())
+            {
+                var author01 = new Author { Id = 1, FName = "eric", LName = "Evan" };
+                context.Authors.Add(author01);
+
+                var author02 = new Author { Id = 2, FName = "Yousef", LName = "ALi" };
+                context.Authors.Add(author02);
+
+                var author03 = new Author { Id = 3, FName = "Sra", LName = "ALi" };
+                context.Authors.Add(author03);
+
+                author03.FName = "Sara";
+                context.SaveChanges();
+            }
+        }
+        private static void RunRelatedData()
+        {
+
+            using (var context = new AppDbContext())
+            {
+                var author01 = context.Authors.FirstOrDefault(x => x.Id == 1);
+
+                author01.Books.Add(new Book
+                {
+                    Id = 1,
+                    Title = "Domain-Driven Design: Tackling Complexity in the Heart of Software"
+                });
+
+                context.SaveChanges();
+            }
+        }
+
+    }
+}
---
+++ EffictentUpdating/Program.cs
@@ -0,0 +1,100 @@
+﻿
+using C01.SqlQuery.Data;
+using C06.Interceptors.Helpers;
+using Microsoft.EntityFrameworkCore;
+
+namespace EffictentUpdating
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Increase_Book_Price_By_10Percent_For_Author1_Typical_Implementation();
+            //Increase_Book_Price_By_10Percent_For_Author1_EF7AnUp_Implementation();
+            //Delete_Book_With_Title_Start_With_Book_EF7AnUp_Implementation();
+            Increase_Book_Price_By_10Percent_For_Author1_EF7AnUp_RawSql();
+
+        }
+
+        private static void Increase_Book_Price_By_10Percent_For_Author1_Typical_Implementation()
+        {
+            Console.WriteLine($">>>> Sample: {nameof(Increase_Book_Price_By_10Percent_For_Author1_Typical_Implementation)}");
+            Console.WriteLine();
+
+            DatabaseHelper.RecreateCleanDatabase();
+            DatabaseHelper.PopulateDatabase();
+
+            using (var context = new AppDbContext())
+            {
+                var author1Books = context.Books.Where(x => x.AuthorId == 1);
+
+                foreach (var book in author1Books) // Deffered Execution
+                {
+                    book.Price *= 1.1m;
+                }
+                context.SaveChanges();
+
+            }
+
+            Console.ReadKey();
+        }
+
+        private static void Increase_Book_Price_By_10Percent_For_Author1_EF7AnUp_Implementation()
+        {
+            Console.WriteLine($">>>> Sample: {nameof(Increase_Book_Price_By_10Percent_For_Author1_Typical_Implementation)}");
+            Console.WriteLine();
+
+            DatabaseHelper.RecreateCleanDatabase();
+            DatabaseHelper.PopulateDatabase();
+
+            using (var context = new AppDbContext())
+            {
+                var author1Books = context.Books.Where(x => x.AuthorId == 1);
+                context.Books.Where(x => x.AuthorId == 1)
+                    .ExecuteUpdate(b => b
+                    .SetProperty(p => p.Price, p => p.Price * 1.1m));
+
+            }
+
+            Console.ReadKey();
+        }
+
+        private static void Delete_Book_With_Title_Start_With_Book_EF7AnUp_Implementation()
+        {
+            Console.WriteLine($">>>> Sample: {nameof(Increase_Book_Price_By_10Percent_For_Author1_Typical_Implementation)}");
+            Console.WriteLine();
+
+            DatabaseHelper.RecreateCleanDatabase();
+            DatabaseHelper.PopulateDatabase();
+
+            using (var context = new AppDbContext())
+            {
+                var author1Books = context.Books.Where(x => x.AuthorId == 1);
+
+                context.Books.Where(x => x.Title.StartsWith("Book"))
+                     .ExecuteDelete();
+
+            }
+
+            Console.ReadKey();
+        }
+
+        private static void Increase_Book_Price_By_10Percent_For_Author1_EF7AnUp_RawSql()
+        {
+            Console.WriteLine($">>>> Sample: {nameof(Increase_Book_Price_By_10Percent_For_Author1_Typical_Implementation)}");
+            Console.WriteLine();
+
+            DatabaseHelper.RecreateCleanDatabase();
+            DatabaseHelper.PopulateDatabase();
+
+            using (var context = new AppDbContext())
+            {
+                var author1Books = context.Books.Where(x => x.AuthorId == 1);
+
+
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
